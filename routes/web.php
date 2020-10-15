3d20812c890784ca6707445f6c7ccbeba3c9c08a
CianOToole <cianotoole4@gmail.com>
2020-10-15 23:42:06 +0100

    added stock info and DB table and set up the routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StockController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('stocks',[StockController::class,'index']);
+Route::get('/stocks/create', [TodoController::class, 'create'])->name('stocks.create');
+Route::post('/stocks', [TodoController::class, 'store'])->name('stocks.store'); // this will be making a post request
+Route::get('/stocks/{id}', [TodoController::class, 'show'])->name('stocks.show');
+Route::get('/stocks/{id}/edit', [TodoController::class, 'edit'])->name('stocks.edit');
+Route::put('/stocks/{id}', [TodoController::class, 'update'])->name('stocks.update'); //making a put request
+Route::delete('/stocks/{id}', [TodoController::class, 'destroy'])->name('stocks.destroy'); //making a delete request
