26ea3a396f731c2ec5aabddae0124616094e1408
CianOToole <cianotoole4@gmail.com>
2020-10-21 02:53:45 +0100

    added tables and routes for stocks

---
+++ database/migrations/2020_10_20_161810_create_trades_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trades', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price_at_order', 8, 2);
+            $table->decimal('amount', 8, 2);
+            $table->boolean('sellOrBuy');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('stock_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trades');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StockController;
+use App\Http\Controllers\TradeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,9 +21,18 @@ Route::get('/', function () {
 
 
 Route::get('stocks',[StockController::class,'index']);
-Route::get('/stocks/create', [TodoController::class, 'create'])->name('stocks.create');
-Route::post('/stocks', [TodoController::class, 'store'])->name('stocks.store'); // this will be making a post request
-Route::get('/stocks/{id}', [TodoController::class, 'show'])->name('stocks.show');
-Route::get('/stocks/{id}/edit', [TodoController::class, 'edit'])->name('stocks.edit');
-Route::put('/stocks/{id}', [TodoController::class, 'update'])->name('stocks.update'); //making a put request
-Route::delete('/stocks/{id}', [TodoController::class, 'destroy'])->name('stocks.destroy'); //making a delete request
+Route::get('/stocks/create', [StockController::class, 'create'])->name('stocks.create');
+Route::post('/stocks', [StockController::class, 'store'])->name('stocks.store'); // this will be making a post request
+Route::get('/stocks/{id}', [StockController::class, 'show'])->name('stocks.show');
+Route::get('/stocks/{id}/edit', [StockController::class, 'edit'])->name('stocks.edit');
+Route::put('/stocks/{id}', [StockController::class, 'update'])->name('stocks.update'); //making a put request
+Route::delete('/stocks/{id}', [StockController::class, 'destroy'])->name('stocks.destroy'); //making a delete request
+
+
+Route::get('trades',[TradeController::class,'index']);
+Route::get('/trades/create', [TradeController::class, 'create'])->name('trades.create');
+Route::post('/trades', [TradeController::class, 'store'])->name('trades.store'); // this will be making a post request
+Route::get('/trades/{id}', [TradeController::class, 'show'])->name('trades.show');
+Route::get('/trades/{id}/edit', [TradeController::class, 'edit'])->name('trades.edit');
+Route::put('/trades/{id}', [TradeController::class, 'update'])->name('trades.update'); //making a put request
+Route::delete('/trades/{id}', [TradeController::class, 'destroy'])->name('trades.destroy'); //making a delete request
