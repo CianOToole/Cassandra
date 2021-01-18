8e339003df9dbffec1d64941f0614d2af1d687a2
Yvan <48021638+Botch-99@users.noreply.github.com>
2021-01-18 15:02:28 +0100

    Quoting added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -32,10 +32,15 @@
 
                         <div class="col-12" >
                             <div class="" style="float: right">
-                                @if($user = Auth::user()->id == $post->user_id)
-                                    <button>quote</button>
-                                    <button>edit</button>
-                                    
+
+                                <button id="{{ $post->id }}"  onClick="quote(this.id)" class="btn btn-primary">
+                                    <i class="fas fa-quote-right"></i>
+                                </button>
+
+                                @if($user = Auth::user()->id == $post->user_id)                                    
+                                    <button class="btn btn-dark">
+                                        <i class="fas fa-pen"></i>
+                                    </button>                                    
                                     <form style="display:inline-block" method="POST" action="{{ route( 'topic.posts.destroy', [$topic_id, $post->id]) }}">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value=" {{ csrf_token() }} ">                                        
@@ -69,7 +74,7 @@
                         </div>
 
                         <div class="col-md-12">
-                            <p>{{$post->post}}</p>
+                            <p class="{{ $post->id }}">{{$post->post}}</p>
                         </div>
 
                     </div>
@@ -84,7 +89,7 @@
                 <form method="POST" action="{{ route( 'topic.posts.store', [$topic_id]) }}">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}" >
                     <div class="form-group">
-                      <textarea type="text" class="form-control" id="post" name="post" placeholder="Post what you have to say, nobody is here to judge you.."></textarea>
+                      <textarea type="text" class="form-control" id="post_txt" name="post" placeholder="Post what you have to say, nobody is here to judge you.."></textarea>
                     </div>
                     <button type="submit" class="btn btn-primary">Post</button>
                   </form>
